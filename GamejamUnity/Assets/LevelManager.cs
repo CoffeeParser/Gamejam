1bd58239a29673a2e07ba1e3b89226cb48cbae3e
DESKTOP-E8OEJA7\schnu <schnubbi2704@web.de>
2018-07-19 10:36:35 +0200

    Add UI

---
+++ GamejamUnity/Assets/LevelManager.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using System;
+
+public class LevelManager : MonoBehaviour {
+
+    public static LevelManager instance;
+
+    private static int IndexOfLastLoadedScene = -1;
+    private static string StringOfLastLoadedScene = "";
+
+
+    //public UnityEvent levelIsOver;
+    //public UnityEvent levelReload;
+
+    [Header("SceneFadeAnimation")]
+    public Texture2D fadeOutTexture;    // ScreenOverlay
+    public float fadeSpeed = 0.8f;      // FadeOutTime 
+
+    private int drawDeath = -1000;      // TextureLayerOrder -1000 it renders on top 
+    private float alpha = 1.0f;         // Texture AlphaValue
+    private int fadedir = -1;           // the direction to fade in = -1 out = 1
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    //FADEEFFEKT
+    private void OnGUI()
+    {      
+        // fade out/in the alpha value using a direction, a speed and time.deltatime 
+        alpha += fadedir * fadeSpeed * Time.deltaTime;
+        // force clamp the number bewteen 0, 1 because of GUIColor
+        alpha = Mathf.Clamp01(alpha);
+
+        // set Color of GUI
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);            // Set alpha value
+        GUI.depth = drawDeath;                                                          // render Texture
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeOutTexture);   // Let fir Texture to Screen
+        
+    }
+
+    public float BeginFade (int direction)
+    {
+        Debug.Log("Begin Fade");
+        fadedir = direction;
+        return (fadeSpeed); // return for applicationen LoadLevel
+    }
+
+    private void Start()
+    {
+        Debug.Log("Start");
+        BeginFade(-1);
+    }
+
+
+    private void OnLevelWasLoaded(int level)
+    {
+
+        //alpha = 1;
+        //BeginFade(-1);
+    }
+
+    public void FadeIn()
+    {
+        StartCoroutine(IEFadeIn());
+    }
+    public IEnumerator IEFadeIn()
+    {
+        Debug.Log("Fade IN Change");
+        float fadeTime = BeginFade(-1);
+        yield return new WaitForSeconds(fadeTime);
+    }
+
+    public void FadeOut(int sceneIndex)
+    {
+        StartCoroutine(IEFadeOut(sceneIndex));
+    }
+    public IEnumerator IEFadeOut(int sceneIndex = -1)
+    {
+        Debug.Log("Fade OUT Change");
+        float fadeTime = BeginFade(1);
+        yield return new WaitForSeconds(fadeTime);
+        if(sceneIndex >= 0)
+        {
+            SceneManager.LoadScene(sceneIndex, LoadSceneMode.Additive);
+        }
+        //StartCoroutine(FadeIn());
+    }
+
+
+    public int GetIndexOfCurrentScene()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+
+        Debug.Log("Buildindex of CurrentScene is: " + index);
+        return index;
+    }
+
+    public int GetIndexOfLastLoadedScene()
+    {
+        Debug.Log("Buildindex of CurrentScene is: " + IndexOfLastLoadedScene);
+        return IndexOfLastLoadedScene;
+    }
+
+    public void LoadByIndex(int sceneIndex)
+    {
+        FadeOut(sceneIndex);
+        IfLevelIsOver();
+        Invoke("FadeIn",1);
+        IndexOfLastLoadedScene = sceneIndex;
+    }
+
+    public void LoadByString(string sceneString)
+    {
+        IfLevelIsOver();
+        SceneManager.LoadScene(sceneString, LoadSceneMode.Additive);
+        StringOfLastLoadedScene = sceneString;
+    }
+
+    public void IfLevelIsOver()
+    {
+        if(IndexOfLastLoadedScene >= 0)
+        {
+            try
+            {
+                //Debug.Log("Current SceneNumber: " + IndexOfLastLoadedScene);
+                SceneManager.UnloadSceneAsync(IndexOfLastLoadedScene);
+            }
+            catch (Exception e)
+            {
+                Debug.Log("Scene was not Found");
+            }
+        }
+        else if (StringOfLastLoadedScene != "")
+        {
+            try
+            {
+                SceneManager.UnloadSceneAsync(StringOfLastLoadedScene);
+            }
+            catch (Exception e)
+            {
+                Debug.Log("Scene was not Found");
+            }
+        }
+    }
+     
+    public void IfLevelHasToReload()
+    {
+
+    }
+    /// CallBack bei Scene Loaded ? / 
+    /// LadeBalken 
+    /// SceneFader
+
+}
---
+++ GamejamUnity/Assets/UIController.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+
+public class UIController : MonoBehaviour {
+
+
+    [Header("MAIN MENU")]
+    public GameObject MainMenu;
+    public Button OpenMenu;
+    public Button StartButton;
+    public Button Options;
+    public Button HighScore;
+    public Button Quit;
+
+    [Header("Options Menu")]
+    public GameObject OptionsMenu;
+    [Header("Audio")]
+    public Slider MasterVolume;
+    public Slider MusicVolume;
+    public Slider SFXVolume;
+
+    [Header("Video")]
+    public Slider Bightness;
+    public Toggle Antialiasing;
+    public Toggle Toggel;
+
+    [Header("GamePlay")]
+    public Slider ShakeSensitivity;
+
+    [Header("OptionsBack")]
+    public Button OptionsBack;
+
+
+
+
+
+    private bool MenuisActive = false;
+    private bool OptionsisActive = false;
+
+
+
+    // Use this for initialization
+    void Start () {
+        MainMenu.SetActive(MenuisActive);
+        OptionsMenu.SetActive(OptionsisActive);
+
+        // Main 
+        OpenMenu.onClick.AddListener(OpenTheMenu);
+        StartButton.onClick.AddListener(StartTheGame);
+        Options.onClick.AddListener(OpenTheOptions);
+        HighScore.onClick.AddListener(OpenHighScore);
+        Quit.onClick.AddListener(QuitGame);
+
+        // Options
+        // Audio
+        MasterVolume.value = 1;
+        MusicVolume.value = 1;
+        SFXVolume.value = 1;
+        // Video
+        Bightness.value = 50;
+
+        Antialiasing.isOn = false;
+        Antialiasing.onValueChanged.AddListener(delegate
+        {
+            AntialiasingToggleHasChanges(Antialiasing);
+        });
+        OptionsBack.onClick.AddListener(OpenTheMenu);
+
+        Toggel.isOn = false;
+        // GamePlay
+        ShakeSensitivity.value = 1;
+
+
+        LevelManager.instance.LoadByIndex(1);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {     
+        MainMenu.SetActive(MenuisActive);
+        OptionsMenu.SetActive(OptionsisActive);
+    }
+
+
+    /// <summary>
+    /// MAIN MENU FUNCTIONS
+    /// </summary>
+    public void OpenTheMenu()
+    {
+        OptionsisActive = false;
+
+        if (!MenuisActive)
+        {
+            MenuisActive = true;
+            //Debug.Log("openMenu" + MenuisActive);
+        }
+        else
+        {
+            MenuisActive = false;
+            //Debug.Log("closeMenu" + MenuisActive);
+        }
+    }
+
+    private void StartTheGame()
+    {
+        // LoadLevel
+        MenuisActive = false;
+        OptionsisActive = false;
+        
+        LevelManager.instance.LoadByIndex(0);
+
+        //GetComponent<LevelManager>().LoadByIndex(0);
+        /*
+        try
+        {
+            GetComponent<LevelManager>().LoadByIndex(0);
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Cant Load Scene");
+        }
+        */
+    }
+    
+
+    private void OpenTheOptions()
+    {
+        MenuisActive = false;
+        if (!OptionsisActive)
+        {
+            OptionsisActive = true;
+            Debug.Log("openMenu" + OptionsisActive);
+        }
+        else
+        {
+            OptionsisActive = false;
+            Debug.Log("closeMenu" + OptionsisActive);
+        }
+    }
+
+    private void OpenHighScore()
+    {
+        MenuisActive = false;
+        OptionsisActive = false;
+        Debug.Log("HighScore Menu is Not Implemetet");
+    }
+
+    private void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    /// <summary>
+    /// OptionsMenu Functions
+    /// </summary>
+
+    // Audio
+    public float GetMasterVolumeValue()
+    {
+        return MasterVolume.value;
+    }
+    public float GetMusicVolumeValue()
+    {
+        return MasterVolume.value;
+    }
+    public float GetSFXVolumeValue()
+    {
+        return MasterVolume.value;
+    }
+
+    // Video
+    public float GetBightnessValue()
+    {
+        return Bightness.value;
+    }
+
+    public bool AntialiasingValue()
+    {
+        return Antialiasing.isOn;
+    }
+
+    private void AntialiasingToggleHasChanges(Toggle antialiasing)
+    {
+        Debug.Log(Antialiasing.isOn);
+    }
+
+    // GamePlay 
+    public float GetShakeSensitivity()
+    {
+        return ShakeSensitivity.value;
+    }
+
+
+
+}
