8edda4ecfc517758708ebeaa144e2d777b8b208f
Denis Dik <denis.dik@hs-furtwangen.de>
2018-07-19 12:18:05 +0200

    Added first set of shaders and materials

---
+++ GamejamUnity/Assets/Shaders/DitheringShaders/Scripts/DitheringEffect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+[AddComponentMenu("Image Effects/Other/Dithering")]
+public class DitheringEffect : MonoBehaviour
+{
+    public int ColorCount = 4;
+    public int PaletteHeight = 64;
+    public Texture PaletteTexture;
+    public int DitherSize = 8;
+    public Texture DitherTexture;
+    public Material DitherMaterial;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        DitherMaterial.SetFloat("_ColorCount", ColorCount);
+        DitherMaterial.SetFloat("_PaletteHeight", PaletteHeight);
+        DitherMaterial.SetTexture("_PaletteTex", PaletteTexture);
+        DitherMaterial.SetFloat("_DitherSize", DitherSize);
+        DitherMaterial.SetTexture("_DitherTex", DitherTexture);
+        Graphics.Blit(source, DitherMaterial);
+    }
+}
---
+++ GamejamUnity/Assets/Shaders/DitheringShaders/Scripts/DitheringEffectSimple.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+[AddComponentMenu("Image Effects/Other/Dithering Simple")]
+public class DitheringEffectSimple : MonoBehaviour
+{
+    public int ColorCount = 4;
+    public int PaletteHeight = 64;
+    public Texture PaletteTexture;
+    public Material DitherMaterial;
+
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        DitherMaterial.SetFloat("_ColorCount", ColorCount);
+        DitherMaterial.SetFloat("_PaletteHeight", PaletteHeight);
+        DitherMaterial.SetTexture("_PaletteTex", PaletteTexture);
+        Graphics.Blit(source, DitherMaterial);
+    }
+}
