2e043732f8e74b8e674b06fd1e628254bde92e79
DESKTOP-E8OEJA7\schnu <schnubbi2704@web.de>
2018-07-24 20:49:23 +0200

    Add PatientSceneLoader

--- GamejamUnity/Assets/ScreenViewHandler.cs
+++ GamejamUnity/Assets/ScreenViewHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,11 +11,17 @@ public class ScreenViewHandler : MonoBehaviour {
     public Button EnterMapMenu;
     public GameObject MapViewGameObject;
     public GameObject EvilScreenGameObject;
+    public GameObject PatientScreenGameObject;
 
     public GameObject DialogField;
     public Text dialogFieldtext;
 
     private GameState _gameState;
+
+    public Button SkipDialogBtn;
+
+
+
 
     void Awake () {
         _gameState = GameObject.FindGameObjectWithTag("GlobalLifeTime").GetComponent<GameState>();
@@ -25,10 +32,18 @@ public class ScreenViewHandler : MonoBehaviour {
             MapViewGameObject.SetActive(true);
             StartScreen.SetActive(false);
         });
+
+        SkipDialogBtn.onClick.AddListener(SkipEvilDialogScreen);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void SkipEvilDialogScreen()
+    {
+        EvilScreenGameObject.SetActive(false);
+        PatientScreenGameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
@@ -36,7 +51,7 @@ public class ScreenViewHandler : MonoBehaviour {
     {
         MapViewGameObject.SetActive(false);
         EvilScreenGameObject.SetActive(true);
-      
+        DialogField.SetActive(true);
         dialogFieldtext.text = _gameState.Begruessung;
     }
 
