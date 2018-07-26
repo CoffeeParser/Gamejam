fadda0123690345b5e59812a898971715238bcc3
RAWBERT\Rawbert <doehler.robert@gmail.com>
2018-07-26 11:08:57 +0200

    UI_rescale + Anordnung Theraphiescenen

---
+++ GamejamUnity/Assets/Scripts/UI_Graphic_Rescaler.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+[RequireComponent(typeof(Image))]
+public class UI_Graphic_Rescaler : MonoBehaviour {
+
+    public float widthPercentage;
+    public float heightPercentage;
+    public float offsetY_percentage;
+    public float offsetX_percentage;
+    private Image image;
+    private RectTransform rect;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    // Use this for initialization
+    public void Init () {
+        image = GetComponent<Image>();
+        rect = GetComponent<RectTransform>();
+	}
+	
+    public void rescaleImageBasedOnHeight()
+    {
+        float newHeight = GetPercentageHeight(heightPercentage);
+        rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, newHeight);
+
+        float newWidth = newHeight * GetAspectRatioHeight();
+        rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, newWidth);
+    }
+
+    public void rescaleImageBasedOnWidth()
+    {
+        //float newWidth = GetPercentageWidth(heightPercentage); 
+        //rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, newWidth);
+
+        //float newHeight = newWidth * GetAspectRatioHeight();
+        //rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, newHeight);
+    }
+
+    public void setPercentageOffset()
+    {
+        Vector3 newPos = new Vector3(GetPercentageWidth(offsetX_percentage), GetPercentageHeight(offsetY_percentage), 0);
+        rect.SetPositionAndRotation(newPos, Quaternion.identity);
+    }
+
+    public float GetAspectRatioHeight()
+    {
+        return image.sprite.rect.width / image.sprite.rect.height;
+    }
+
+    public float GetAspectRatioWidth()
+    {
+        return image.sprite.rect.height / image.sprite.rect.width;
+    }
+
+    public float GetPercentageWidth(float percentage)
+    {
+        return Camera.main.pixelWidth * percentage;
+    }
+
+    public float GetPercentageHeight(float percentage)
+    {
+        return Camera.main.pixelHeight * percentage;
+    }
+
+    public void ApplyAllResizeObjs()
+    {
+        UI_Graphic_Rescaler[] resizeObjs = GetComponentsInChildren<UI_Graphic_Rescaler>();
+
+        foreach (UI_Graphic_Rescaler resizeObj in resizeObjs)
+        {
+            if(resizeObj != this){
+                resizeObj.Init();
+                resizeObj.rescaleImageBasedOnHeight();
+                resizeObj.setPercentageOffset();
+            }
+        }
+    }
+}
+
+
+[CustomEditor(typeof(UI_Graphic_Rescaler))]
+public class ObjectBuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        UI_Graphic_Rescaler resizer = (UI_Graphic_Rescaler)target;
+        if (GUILayout.Button("Resize depending width"))
+        {
+            resizer.Init();
+            resizer.rescaleImageBasedOnWidth();
+        }
+        if (GUILayout.Button("Resize depending height"))
+        {
+            resizer.Init();
+            resizer.rescaleImageBasedOnHeight();
+        }
+        if (GUILayout.Button("Set percentage Offset"))
+        {
+            resizer.Init();
+            resizer.setPercentageOffset();
+        }
+        if (GUILayout.Button("Apply all resize Objs"))
+        {
+            resizer.ApplyAllResizeObjs();
+        }
+    }
+}
